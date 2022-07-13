788fd2b19ffd143b8da2a7c57dba37e0c4576229
mr-delta <leonardogalencar444@gmail.com>
2022-07-13 11:29:15 -0300

    alterações navbar, acesso rápido e adição do footer

--- admin/index.php
+++ admin/index.php
@@ -32,7 +32,7 @@
 
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
 
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-etec sticky-top">
         <div class="container">
             <a class="navbar-brand" href="index.php">
                 <img src="../img/logo.png" alt="" width="100px">
@@ -45,12 +45,12 @@
                     <a class="nav-item nav-link active" href="index.php">Home</a>
                     <a class="nav-item nav-link" href="cursos.php">Cursos</a>
                     <a class="nav-item nav-link" href="http://etecitu.com.br/2020/" target="_blank">EPA Virtual</a>
-                    <a class="nav-item nav-link" href="?p=produto/consultar">Notícias</a>
-                    <a class="nav-item nav-link" href="?p=produto/consultar">Processo Seletivo</a>
-                    <a class="nav-item nav-link" href="?p=produto/consultar">Vestibulinho</a>
-                    <a class="nav-item nav-link" href="?p=produto/consultar">Calendário</a>
-                    <a class="nav-item nav-link" href="?p=produto/consultar">Plano Escolar</a>
-                    <a class="nav-item nav-link" href="?p=produto/consultar">Contato</a>
+                    <a class="nav-item nav-link" href="noticias.php">Notícias</a>
+                    <a class="nav-item nav-link" href="processoseletivo.php">Processo Seletivo</a>
+                    <a class="nav-item nav-link" href="vestibulinho.php">Vestibulinho</a>
+                    <a class="nav-item nav-link" href="calendario.php">Calendário</a>
+                    <a class="nav-item nav-link" href="planoescolar.php">Plano Escolar</a>
+                    <a class="nav-item nav-link" href="contato.php">Contato</a>
                 </div>
             </div>
         </div>
@@ -131,8 +131,7 @@
                 <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
             </div>
             <div class="col-md-5">
-                <img src="https://colegioprudente.com.br/novo/wp-content/uploads/2021/12/home-info3.jpg" alt="" width="500" height="500">
-
+                <img src="https://colegioprudente.com.br/novo/wp-content/uploads/2021/12/home-info3.jpg" alt="" style="max-width: 100%;">
             </div>
         </div>
 
@@ -209,41 +208,39 @@
             </div>
         </div>
         <div class="container marketing mb-padrao">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="cards">
-                        <img class="cards-img" src="../img/cadeado.png" alt="Bricks">
-                        <div class="cards-img-overlay cards-img-overlay-primary">
-                            <div class="cards-img-title">NSA</div>
-                            <p class="cards-img-description">
-                                
-                            </p>
+            <div class="row" style="justify-content: center;">
+                <div class="col-lg-3">
+                    <a href="https://nsa.cps.sp.gov.br/" class="box-acesso" target="_blank">
+                        <div class="box-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-key icon" viewBox="0 0 16 16">
+                                <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"></path>
+                                <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
+                            </svg>
                         </div>
-                    </div>
+                        <h2>NSA</h2>
+                    </a>
                 </div>
-                <div class="col-lg-4">
-                    <div class="cards">
-                        <img class="cards-img" src="../img/teams.png" alt="Bricks">
-                        <div class="cards-img-overlay cards-img-overlay-primary">
-                            <div class="cards-img-title">Microsoft Teams</div>
-                            <p class="cards-img-description">
-                                
-                            </p>
+                <div class="col-lg-3">
+                    <a href="https://nsa.cps.sp.gov.br/" class="box-acesso" target="_blank">
+                        <div class="box-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-windows icon" viewBox="0 0 16 16">
+                                <path d="M6.555 1.375 0 2.237v5.45h6.555V1.375zM0 13.795l6.555.933V8.313H0v5.482zm7.278-5.4.026 6.378L16 16V8.395H7.278zM16 0 7.33 1.244v6.414H16V0z"></path>
+                            </svg>
                         </div>
-                    </div>
+                        <h2>Teams</h2>
+                    </a>
                 </div>
-                <div class="col-lg-4">
-                    <div class="cards">
-                        <img class="cards-img" src="https://www.etectupa.com.br/theme/assets/images/mini-medio.jpg" alt="Bricks">
-                        <div class="cards-img-overlay cards-img-overlay-primary">
-                            <div class="cards-img-title">Facebook</div>
-                            <p class="cards-img-description">
-                                
-                            </p>
+                <div class="col-lg-3">
+                    <a href="https://nsa.cps.sp.gov.br/" class="box-acesso" target="_blank">
+                        <div class="box-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-facebook icon" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+                            </svg>
                         </div>
-                    </div>
+                        <h2>Facebook</h2>
+                    </a>
                 </div>
-            </div><!-- /.row -->
+            </div> <!-- /.row -->
         </div>
 
         <!-- NOTICIAS !-->
@@ -281,13 +278,59 @@
                 </div>
             </div>
         </section>
-
-
-
-        <!-- FOOTER !-->
-
-
     </div>
+
+    <!-- FOOTER !-->
+    <footer>
+        <div class="container">
+            <div class="footer-links text-left">
+                <span>
+                    <a href="contato.php">Contato</a>
+                </span>
+                <span>
+                    <a href="https://www.vestibulinhoetec.com.br/home/">Vestibulinho</a>
+                </span>
+                <span>
+                    <a href="processoseletivo.php">Processo Seletivo</a>
+                </span>
+                <span>
+                    <a href="https://nsa.cps.sp.gov.br/">NSA</a>
+                </span>
+                <span>
+                    <a href="http://www.etecitu.com.br/2019/arquivos/index/manualTCC.pdf">Manual do TCC</a>
+                </span>
+                <span>
+                    <a href="secretariaacademica.php">Secretaria Acadêmica</a>
+                </span>
+                <span>
+                    <a href="planogestao.php">Plano Plurianual de Gestão</a>
+                </span>
+                <span>
+                    <a href="https://www.facebook.com/EtecdeItu" class="footer-icons" target="_blank">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                    <a href="https://www.instagram.com/etecmartinho/" class="footer-icons" target="_blank">
+                        <i class="bi bi-instagram"></i>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCgiX2_tK8Imaf5OpcQNK-yA" class="footer-icons" target="_blank">
+                        <i class="bi bi-youtube"></i>
+                    </a>
+                </span>
+            </div>
+            <hr style="color: #252a3c; margin: 1rem 0;">
+            <div class="row align-items-center">
+                <div class="col-9 text-center">
+                    <p>Copyright © 2022 - Etec Martinho di Ciero - Centro Paula Souza - Avenida Barata Ribeiro, 410 Vila Prudente de Moraes - Itu/SP</p>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="../img/logo.png" alt="" class="mb-2" style="width: 80px;">
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <script src="../js/script.js"></script>
