698c188afc0aa2a93d2181f16a0bf52e142c30a0
mr-delta <leonardogalencar444@gmail.com>
2022-07-21 13:21:58 -0300

    Modularização do projeto em PHP

---
+++ admin/footer.php
@@ -0,0 +1,57 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 d-flex">
+                    <ul class="nav footer-links justify-content-center justify-content-md-between">
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="contato.php">Contato</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="https://www.vestibulinhoetec.com.br/home/">Vestibulinho</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="processoseletivo.php">Processo Seletivo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="https://nsa.cps.sp.gov.br/">NSA</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="http://www.etecitu.com.br/2019/arquivos/index/manualTCC.pdf">Manual do TCC</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="secretariaacademica.php">Secretaria Acadêmica</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2" href="planogestao.php">Plano Plurianual de Gestão</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="nav footer-links justify-content-center justify-content-md-end">
+                        <li>
+                            <a class="nav-link px-2" href="https://www.facebook.com/EtecdeItu" class="footer-icons" target="_blank">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+                            <a class="nav-link px-2" href="https://www.instagram.com/etecmartinho/" class="footer-icons" target="_blank">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                            <a class="nav-link px-2" href="https://www.youtube.com/channel/UCgiX2_tK8Imaf5OpcQNK-yA" class="footer-icons" target="_blank">
+                                <i class="bi bi-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <hr style="color: #252a3c; margin: 1rem 0;">
+
+            <div class="row align-items-center">
+                <div class="col-md-9 text-center text-md-start">
+                    <p>Copyright © 2022 - Etec Martinho di Ciero - Centro Paula Souza - Avenida Barata Ribeiro, 410 Vila Prudente de Moraes - Itu/SP</p>
+                </div>
+                <div class="col-md-3 text-center text-md-end">
+                    <img src="../img/logo.png" alt="" class="mb-2" style="width: 80px;">
+                </div>
+            </div>
+        </div>
+    </footer>
